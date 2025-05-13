f6b9e6c9a6ec6e186a1b96556a21340292d9de2b
timothyn <tnguyen_42@yahoo.com>
2025-05-13 12:30:37 -0400

    Finish Create Action

--- controllers/applications.js
+++ controllers/applications.js
@@ -12,7 +12,6 @@ router.use(ensureLoggedIn);
 // index action
 // GET /applications
 router.get('/', async (req, res) => {
-  // req.user is the logged in user's document
   res.render('applications/index.ejs');
 });
 
@@ -22,16 +21,25 @@ router.get('/new', (req, res) => {
   res.render('applications/new.ejs');
 });
 
-
-// GET /applications/new
-// Example of a protected route
-router.get('/new', ensureLoggedIn, (req, res) => {
-  res.send('Create a unicorn!');
+// create route/action
+// POST /applications
+router.post('/', async (req, res) => {
+  // req.user is the logged in user's document
+  req.user.applications.push(req.body);
+  await req.user.save();
+  res.redirect('/applications');
 });
+
 
 module.exports = router;
 
 // Example of a non-protected route
 // router.get('/', (req, res) => {
 //   res.send('List of all applications - not protected');
+// });
+
+// GET /applications/new
+// Example of a protected route
+// router.get('/new', ensureLoggedIn, (req, res) => {
+//   res.send('Create a unicorn!');
 // });
